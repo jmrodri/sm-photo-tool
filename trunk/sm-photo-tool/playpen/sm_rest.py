f76d4367466ce965a28aab3a5e12a84deea572b0
jesus m rodriguez <jmrodri@gmail.com>
2007-03-14 02:28:58 +0000

    I got dynamic method calling to work which allows me to do things like:
    sm = Smugmug()
    sm.smugmug.login.withPassword(EmailAddress="....", Password="fooo")
    
    REST requires the method name, and the parameter names.  This is pretty
    cool. This isn't really possible in java.

--- trunk/sm-photo-tool/playpen/sm_rest.py
+++ trunk/sm-photo-tool/playpen/sm_rest.py
@@ -6,6 +6,19 @@ from xml.dom.ext.reader import Sax2
 #map = {param1=>value1,param2=>value2}
 #Smugmug.method(map)
 
+class _Method:
+    # some magic to bind an XML-RPC method to an RPC server.
+    # supports "nested" methods (e.g. examples.getStateName)
+    def __init__(self, send, name):
+        self.__send = send
+        self.__name = name
+    def __getattr__(self, name):
+        return _Method(self.__send, "%s.%s" % (self.__name, name))
+    def __call__(self, **args):
+        print "__name: %s" % self.__name
+        print "args: " + str(args)
+        return self.__send(self.__name, args)
+
 class Smugmug:
     def __init__(self):
         self.url="https://api.smugmug.com/hack/rest/1.1.1/"
@@ -15,7 +28,13 @@ class Smugmug:
     def addKey(self, call):
         return call + "&APIKey=" + self.apikey
 
-    def invoke(self, method, args):
+    def __getattr__(self, name):
+        # magic method dispatcher
+        print "__getattr__(%s)" % name
+        return _Method(self.__invoke, name)
+
+    def __invoke(self, method, args):
+        print "invoke.args: " + str(args)
         call = self.url + "?method="+ str(method)
         for k, v in args.iteritems():
             call = call + "&" + str(k) + "=" + str(v)
@@ -24,11 +43,9 @@ class Smugmug:
         rsp = urllib.urlopen(call).read()
         print rsp
         reader = Sax2.Reader()
-        #doc = reader.fromString(rsp)
         return rsp
 
 if __name__ == "__main__":
     sm = Smugmug()
-    args = {"EmailAddress":"jmrodri@gmail.com",
-            "Password":"luvLiz69"}
-    sm.invoke("smugmug.login.withPassword", args)
+    sm.smugmug.login.withPassword(EmailAddress="jmrodri@gmail.com",
+                                  Password="****");
