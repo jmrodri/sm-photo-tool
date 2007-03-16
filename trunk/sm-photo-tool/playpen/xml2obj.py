8421f183b0458e374870105e166f1f3d71c9363b
jesus m rodriguez <jmrodri@gmail.com>
2007-03-16 11:34:34 +0000

    changed method calls to start with lowercase as I hate initial caps for method calls.

--- trunk/sm-photo-tool/playpen/xml2obj.py
+++ trunk/sm-photo-tool/playpen/xml2obj.py
@@ -19,7 +19,7 @@ class Element:
         # The element's child element list (sequence)
         self.children = []
         
-    def AddChild(self,element):
+    def addChild(self,element):
         'Add a reference to a child element'
         self.children.append(element)
         
@@ -50,7 +50,7 @@ class Xml2Obj:
         self.root = None
         self.nodeStack = []
         
-    def StartElement(self,name,attributes):
+    def startElement(self,name,attributes):
         'SAX start element even handler'
         # Instantiate an Element object
         element = Element(name.encode(),attributes)
@@ -58,16 +58,16 @@ class Xml2Obj:
         # Push element onto the stack and make it a child of parent
         if len(self.nodeStack) > 0:
             parent = self.nodeStack[-1]
-            parent.AddChild(element)
+            parent.addChild(element)
         else:
             self.root = element
         self.nodeStack.append(element)
         
-    def EndElement(self,name):
+    def endElement(self,name):
         'SAX end element event handler'
         self.nodeStack = self.nodeStack[:-1]
 
-    def CharacterData(self,data):
+    def characterData(self,data):
         'SAX character data event handler'
         if string.strip(data):
             data = data.encode()
@@ -75,19 +75,28 @@ class Xml2Obj:
             element.cdata += data
             return
 
-    def Parse(self,filename):
+    def parse(self,document):
         # Create a SAX parser
-        Parser = expat.ParserCreate()
+        parser = expat.ParserCreate()
 
         # SAX event handlers
-        Parser.StartElementHandler = self.StartElement
-        Parser.EndElementHandler = self.EndElement
-        Parser.CharacterDataHandler = self.CharacterData
+        parser.StartElementHandler = self.startElement
+        parser.EndElementHandler = self.endElement
+        parser.CharacterDataHandler = self.characterData
 
         # Parse the XML File
-        ParserStatus = Parser.Parse(open(filename,'r').read(), 1)
+        status = parser.Parse(document, 1)
         
         return self.root
-    
-#parser = Xml2Obj()
-#element = parser.Parse('sample.xml')
+   
+if __name__ == "__main__":
+    parser = Xml2Obj()
+    sample = """<?xml version="1.0"?>
+<foo>
+  <bar>
+  cat
+  </bar>
+</foo>
+"""
+    element = parser.parse(sample)
+    print element
