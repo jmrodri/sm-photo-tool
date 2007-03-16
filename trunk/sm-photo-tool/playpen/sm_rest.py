e618a88fe98d0f33e301663c7aaf50fb7dcc4179
jesus m rodriguez <jmrodri@gmail.com>
2007-03-16 17:21:37 +0000

    storing a sample albums response, and my current changes to sm_rest

--- trunk/sm-photo-tool/playpen/sm_rest.py
+++ trunk/sm-photo-tool/playpen/sm_rest.py
@@ -1,10 +1,31 @@
 #!/usr/bin/python
 import urllib
-from xml.dom.ext.reader import Sax2
+from xml import xpath
+from xml.dom.minidom import parseString
+
 #url?method=<name>&param1=value1....
 #
 #map = {param1=>value1,param2=>value2}
 #Smugmug.method(map)
+
+
+def getText(nodelist):
+    rc = ""
+    for node in nodelist:
+        if node.nodeType == node.TEXT_NODE:
+            rc = rc + node.data
+        elif node.nodeType == node.ELEMENT_NODE:
+            rc = getText(node[0].childNodes)
+        return rc
+
+class Album:
+    def __init__(self, id, title, category, categoryid, subcategory='', subcategoryid=''):
+        self.id = id
+        self.title = title
+        self.category = category
+        self.categoryid = categoryid
+        self.subcategory = subcategory
+        self.subcategoryid = subcategoryid
 
 class _Method:
     # some magic to bind an XML-RPC method to an RPC server.
@@ -25,7 +46,7 @@ class Smugmug:
         self.apikey="4XHW8Aw7BQqbkGszuFciGZH4hMynnOxJ"
         print self.url
 
-    def addKey(self, call):
+    def __addKey(self, call):
         return call + "&APIKey=" + self.apikey
 
     def __getattr__(self, name):
@@ -38,14 +59,25 @@ class Smugmug:
         call = self.url + "?method="+ str(method)
         for k, v in args.iteritems():
             call = call + "&" + str(k) + "=" + str(v)
-        call = self.addKey(call)
+        #call = self.__addKey(call)
         print call
         rsp = urllib.urlopen(call).read()
-        print rsp
-        reader = Sax2.Reader()
         return rsp
+
+    def findValue(self, doc, path):
+        dom1 = parseString(doc)
+        nodes = xpath.Evaluate(path, dom1)
+        return getText(nodes[0].childNodes)
 
 if __name__ == "__main__":
     sm = Smugmug()
-    sm.smugmug.login.withPassword(EmailAddress="jmrodri@gmail.com",
-                                  Password="****");
+    rsp = sm.smugmug.login.withPassword(EmailAddress="jmrodri@gmail.com",
+                                  Password="****", APIKey=sm.apikey);
+    sessionid = sm.findValue(rsp, "rsp/Login/SessionID")
+    print "rc: " + str(sessionid)
+
+    albums = sm.smugmug.albums.get(SessionID=sessionid)
+    print albums
+
+    rsp1 = sm.smugmug.logout(SessionID=sessionid)
+    print rsp1
