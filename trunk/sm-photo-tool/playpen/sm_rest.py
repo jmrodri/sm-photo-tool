95dc26485ddc0365dac65691c0da770fe606356e
jesus m rodriguez <jmrodri@gmail.com>
2007-03-17 01:10:07 +0000

    Added: album.create, getinfo, delete getimages, images.getinfo

--- trunk/sm-photo-tool/playpen/sm_rest.py
+++ trunk/sm-photo-tool/playpen/sm_rest.py
@@ -2,6 +2,7 @@
 import urllib
 from xml import xpath
 from xml.dom.minidom import parseString
+from time import time
 
 #url?method=<name>&param1=value1....
 #
@@ -36,13 +37,15 @@ class _Method:
     def __getattr__(self, name):
         return _Method(self.__send, "%s.%s" % (self.__name, name))
     def __call__(self, **args):
-        print "__name: %s" % self.__name
-        print "args: " + str(args)
+        #print "__name: %s" % self.__name
+        #print "args: " + str(args)
         return self.__send(self.__name, args)
 
 class Smugmug:
-    def __init__(self):
-        self.url="https://api.smugmug.com/hack/rest/1.1.1/"
+    def __init__(self,version="1.1.1"):
+        #http[s]://api.smugmug.com/hack/rest/1.2.0/
+        #https://api.smugmug.com/hack/rest/1.1.1/
+        self.url="https://api.smugmug.com/hack/rest/%s/" % str(version)
         self.apikey="4XHW8Aw7BQqbkGszuFciGZH4hMynnOxJ"
         print self.url
 
@@ -51,33 +54,69 @@ class Smugmug:
 
     def __getattr__(self, name):
         # magic method dispatcher
-        print "__getattr__(%s)" % name
+        #print "__getattr__(%s)" % name
         return _Method(self.__invoke, name)
 
     def __invoke(self, method, args):
-        print "invoke.args: " + str(args)
+        #print "invoke.args: " + str(args)
         call = self.url + "?method="+ str(method)
         for k, v in args.iteritems():
             call = call + "&" + str(k) + "=" + str(v)
         #call = self.__addKey(call)
         print call
         rsp = urllib.urlopen(call).read()
+        print "urlopen returned: " + str(rsp)
         return rsp
 
     def findValue(self, doc, path):
         dom1 = parseString(doc)
         nodes = xpath.Evaluate(path, dom1)
-        return getText(nodes[0].childNodes)
+        return nodes
+        #return getText(nodes[0].childNodes)
 
 if __name__ == "__main__":
+    # 1.1.1
     sm = Smugmug()
     rsp = sm.smugmug.login.withPassword(EmailAddress="jmrodri@gmail.com",
-                                  Password="****", APIKey=sm.apikey);
-    sessionid = sm.findValue(rsp, "rsp/Login/SessionID")
+                                  Password="****", APIKey=sm.apikey)
+    rsp = sm.findValue(rsp, "rsp/Login/SessionID")
+    sessionid = rsp[0].firstChild.data
     print "rc: " + str(sessionid)
+    
 
-    albums = sm.smugmug.albums.get(SessionID=sessionid)
-    print albums
+    print "calling albums.create"
+    rsp = sm.smugmug.albums.create(SessionID=sessionid, Title="testalbum" + str(time()), CategoryID=0)
+    # should return an id
+    rsp = sm.findValue(rsp, "rsp/Create/Album/@id")
+    albumid = rsp[0].value
+    print albumid
+    
+    print "getinfo"
+    rsp = sm.smugmug.albums.getInfo(SessionID=sessionid,AlbumID=albumid)
+    rsp = sm.findValue(rsp, "rsp/Albums/Album")
+    # should build an Album objects
+    print rsp[0]
+    
+    print "delete"
+    rsp = sm.smugmug.albums.delete(SessionID=sessionid,AlbumID=albumid)
+    rsp = sm.findValue(rsp, "rsp/Delete/Album/Successful");
+    print rsp[0].tagName
 
+
+    print "getimages for album: 2559293"
+    rsp = sm.smugmug.images.get(SessionID=sessionid,AlbumID=2559293)
+    rsp = sm.findValue(rsp, "rsp/Images/Image/@id")
+    # should return array of ids
+    print "count: " + str(len(rsp))
+    print rsp[0].value
+
+    print "getimageinfo"
+    rsp = sm.smugmug.images.getInfo(SessionID=sessionid,ImageID=rsp[0].value)
+    # should return an Image object
+    rsp = sm.findValue(rsp, "rsp/Info/Image")
+    print rsp[0]
+    
+    print "logout"
     rsp1 = sm.smugmug.logout(SessionID=sessionid)
     print rsp1
+
