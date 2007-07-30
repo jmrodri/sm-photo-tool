731a69593831284c8b1e0bc4d1d3922193fd4474
jesus m rodriguez <jmrodri@gmail.com>
2007-07-30 13:01:02 +0000

    adding tarball release and fixed buildtar.sh to use proper filename for src file

--- trunk/sm-photo-tool/buildtar.sh
+++ trunk/sm-photo-tool/buildtar.sh
@@ -3,7 +3,7 @@ NAME=sm-photo-tool
 VERSION=`grep "^version" src/sm_photo_tool.py | awk '{print $3}' | sed 's/\"//g'`
 mkdir /tmp/$NAME-$VERSION/
 cp LICENSE.TXT /tmp/$NAME-$VERSION/
-cp src/sm-photo-tool.py /tmp/$NAME-$VERSION/
+cp src/sm_photo_tool.py /tmp/$NAME-$VERSION/
 cp src/smugmugrc /tmp/$NAME-$VERSION/
 pushd /tmp/ > /dev/null
 tar czf $NAME-$VERSION.tar.gz $NAME-$VERSION/
