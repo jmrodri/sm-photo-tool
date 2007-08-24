b1c4c9a95752e8c8b99efcc2db4bc5946c604164
jesus m rodriguez <jmrodri@gmail.com>
2007-08-24 03:09:48 +0000

    got mkdir to create an album on smugmug TODOS: put username and password in .zmugfs config file

--- trunk/zmugfs/src/zmugfs.py
+++ trunk/zmugfs/src/zmugfs.py
@@ -1,14 +1,15 @@
 import fuse
 from fuse import Fuse
-import os, stat, errno
+import os, stat, errno, time
+import sm_json
 
 fuse.fuse_python_api = (0, 2)
 
 class MyStat(fuse.Stat):
     def __init__(self):
+        self.st_dev = 0
         self.st_mode = 0
         self.st_ino = 0
-        self.st_dev = 0
         self.st_nlink = 0
         self.st_uid = 0
         self.st_gid = 0
@@ -27,11 +28,20 @@ class ZmugFS(Fuse):
         st = MyStat()
         if path == '/':
             st.st_mode = stat.S_IFDIR | 0755
-            st.st_nlink = 2
-        else:
+            st.st_ino = 2
+            st.st_nlink = 1
+            st.st_atime = int(time.time())
+            st.st_mtime = int(time.time())
+            st.st_ctime = int(time.time())
+        elif path == '/foobar':
             st.st_mode = stat.S_IFREG | 0444
             st.st_nlink = 1
-            st.st_size = 10
+            st.st_atime = int(time.time())
+            st.st_mtime = int(time.time())
+            st.st_ctime = int(time.time())
+            st.st_size = len(path)
+        else:
+            return -errno.ENOENT
 
         return st
 
@@ -44,7 +54,18 @@ class ZmugFS(Fuse):
         return 'foo'
 
     def mkdir(self, path, mode):
-        print "mkdir: (%s): (%s)" % (str(path), str(mode))
+        print "mkdir: (%s): (%o)" % (str(path), mode)
+        print "create gallery (%s) on smugmug" % str(path)
+        # get the octal of the mode to see if the album
+        # should be public or not. Need to define a
+        # whether g+r makes it public or not. u+r only
+        # makes it private
+        omode = oct(mode)
+        sm = sm_json.Smugmug()
+        sessionid = sm.loginWithPassword("jmrodri@gmail.com", "***")
+        sm.createAlbum(sessionid, path, Public=0)
+        sm.logout(sessionid)
+        return -errno.ENOSYS
 
 def main():
     usage = """
