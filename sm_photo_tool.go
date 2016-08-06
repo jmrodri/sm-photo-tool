cdfa3d374b0de50b3c39b4b129af122fc4e6ad7e
jesus m. rodriguez <jmrodri@gmail.com>
2016-08-06 14:24:54 -0400

    move commands to smcommands

--- sm_photo_tool.go
+++ sm_photo_tool.go
@@ -1,15 +1,32 @@
+/*
+ * sm_photo_tool.go - update and create smugmug galleries from the
+ *                    command line
+ *
+ * Copyright (C) 2016 Jesus M. Rodriguez
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
 package main
 
-import "github.com/pborman/getopt"
-import "fmt"
-import "os"
-import "sort"
+import (
+	"fmt"
+	"os"
 
-type ListCommand struct {
-	usage         string
-	desc          string
-	valid_options []string
-}
+	"github.com/pborman/getopt"
+)
 
 func main() {
 	//CLI{}.run()
@@ -29,46 +46,4 @@ func main() {
 	lcmd := ListCommand{usage: "Usage", desc: "the list command", valid_options: validOptions}
 	lcmd.validOptions(args)
 	lcmd.doCommand(args)
-}
-
-func (lc *ListCommand) validOptions(args []string) {
-	if len(args) < 2 {
-		fmt.Println(len(args))
-		fmt.Println("print help")
-		os.Exit(1)
-	}
-
-	if len(args) < 3 {
-		if args[1] != "galleries" {
-			fmt.Println("ERROR: requires album or galleries")
-			os.Exit(1)
-		}
-	}
-
-	/*
-		in python this is
-		if args[1] not in valid_options
-	*/
-	i := sort.SearchStrings(lc.valid_options, args[1])
-	if i < len(lc.valid_options) && lc.valid_options[i] != args[1] {
-		fmt.Println("ERROR: valid options are %s", lc.valid_options)
-		os.Exit(1)
-	}
-}
-
-func (lc *ListCommand) doCommand(args []string) {
-	var oid string
-	cmd := args[0]
-
-	if len(args) > 2 {
-		oid = args[1]
-	}
-
-	if cmd == "album" {
-		fmt.Println("list_files(%s)", oid)
-	} else if cmd == "galleries" {
-		fmt.Println("list_galleries()")
-	} else {
-		os.Exit(1)
-	}
 }
---
+++ smcommands.go
@@ -0,0 +1,129 @@
+/*
+ * smcommands.go
+ *
+ * Copyright (C) 2016 Jesus M. Rodriguez
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"sort"
+)
+
+type CliCommand struct {
+	usage     string
+	shortdesc string
+	desc      string
+	name      string
+}
+
+/*
+func Init(name string, usage string, shortdesc string, description string) *CliCommand {
+	return nil
+}*/
+
+func (cc *CliCommand) addCommonOptions() {
+
+}
+
+/************************
+ * UploadCommand
+ ************************/
+type UploadCommand struct {
+	usage     string
+	shortdesc string
+	desc      string
+}
+
+/************************
+ * ListCommand
+ ************************/
+type ListCommand struct {
+	usage         string
+	desc          string
+	valid_options []string
+}
+
+func NewListCommand() *ListCommand {
+	usage := "usage: PROG list <album [albumid] | galleries>"
+	desc := "Lists the files in an ablum, or lists available galleries"
+	return &ListCommand{usage, desc, []string{"album", "galleries"}}
+}
+
+func (lc *ListCommand) validOptions(args []string) {
+	if len(args) < 2 {
+		fmt.Println(len(args))
+		fmt.Println("print help")
+		os.Exit(1)
+	}
+
+	if len(args) < 3 {
+		if args[1] != "galleries" {
+			fmt.Println("ERROR: requires album or galleries")
+			os.Exit(1)
+		}
+	}
+
+	// in python this is
+	// if args[1] not in valid_options
+	i := sort.SearchStrings(lc.valid_options, args[1])
+	if i < len(lc.valid_options) && lc.valid_options[i] != args[1] {
+		fmt.Println("ERROR: valid options are %s", lc.valid_options)
+		os.Exit(1)
+	}
+}
+
+func (lc *ListCommand) doCommand(args []string) {
+	var oid string
+	cmd := args[0]
+
+	if len(args) > 2 {
+		oid = args[1]
+	}
+
+	if cmd == "album" {
+		fmt.Println("list_files(%s)", oid)
+	} else if cmd == "galleries" {
+		fmt.Println("list_galleries()")
+	} else {
+		os.Exit(1)
+	}
+}
+
+/*
+func main() {
+	//CLI{}.run()
+	optName := getopt.StringLong("name", 'n', "", "Your name")
+	optHelp := getopt.BoolLong("help", 0, "Help")
+
+	getopt.Parse()
+	args := getopt.Args()
+
+	if *optHelp {
+		getopt.Usage()
+		os.Exit(0)
+	}
+
+	fmt.Println("Hello " + *optName + "!")
+	validOptions := []string{"album", "galleries"}
+	lcmd := ListCommand{usage: "Usage", desc: "the list command", valid_options: validOptions}
+	lcmd.validOptions(args)
+	lcmd.doCommand(args)
+}
+*/
